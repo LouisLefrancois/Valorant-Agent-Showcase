ead335ed18eed16314eca7cb0f2f2fc1de8d657d
Louislf <lefrancois_louis@yahoo.fr>
2024-06-24 21:52:57 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import logoValorant from "../assets/images/logo-valorant.png";
 
 const Navbar = () => {
   return (
-    <nav className="absolute w-full flex justify-between px-32 py-14 z-10">
+    <nav className="relative w-full flex justify-between px-32 py-14 z-10 bg-transparent navbar">
       <div className="flex items-center space-x-6">
         <a
           href="/"
