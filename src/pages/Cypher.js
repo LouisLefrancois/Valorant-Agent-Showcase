5913c9f637aeb28df3e723c1a26835cc7249a3ac
Louislf <lefrancois_louis@yahoo.fr>
2024-06-26 17:19:09 +0200

    Update Cypher.js

--- src/pages/Cypher.js
+++ src/pages/Cypher.js
@@ -16,8 +16,7 @@ import videoSource from "../assets/videos/valorant-jugement.mp4";
 import thumbVid from "../assets/images/thumbVid.png";
 
 const Cypher = () => {
-  document.title = 'Agent Showcase - Cypher';
-  document.body.style.backgroundImage = `url(${backgroundImage})`;
+  document.title = "Agent Showcase - Cypher";
 
   const [playingAbilityIndex, setPlayingAbilityIndex] = useState(null);
   const [initialVideoPlayed, setInitialVideoPlayed] = useState(false);
@@ -77,49 +76,55 @@ const Cypher = () => {
   };
 
   return (
-    <div className="flexbox">
-      <div className="items-end text-white textContainer">
-        <h2 className="text-2xl mb-1 txtUpper">Agent No. 05</h2>
-        <h1 className="text-9xl font-medium uppercase -ml-1 txtTitle">
-          Cypher
-        </h1>
-        <p className="text-base mb-6 txtDescription">
-          Informateur originaire du Maroc, Cypher est un véritable réseau de
-          surveillance à lui tout seul. Il révèle tous les secrets. Il détecte
-          toutes les manœuvres. Rien n'échappe à Cypher.
-        </p>
-        <div className="flex">
-          {abilities.map((ability, index) => (
-            <img
-              key={index}
-              src={ability.img}
-              className={`w-10 h-10 mr-7 hover:opacity-100 cursor-pointer abilityIcons ${
-                playingAbilityIndex === index ? "opacity-100" : "opacity-60"
-              }`}
-              onClick={() => handleAbilityClick(index)}
-            />
-          ))}
+    <div>
+      <div
+        className="background"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
+      <div className="flexbox">
+        <div className="items-end text-white textContainer">
+          <h2 className="text-2xl mb-1 txtUpper">Agent No. 05</h2>
+          <h1 className="text-9xl font-medium uppercase -ml-1 txtTitle">
+            Cypher
+          </h1>
+          <p className="text-base mb-6 txtDescription">
+            Informateur originaire du Maroc, Cypher est un véritable réseau de
+            surveillance à lui tout seul. Il révèle tous les secrets. Il détecte
+            toutes les manœuvres. Rien n'échappe à Cypher.
+          </p>
+          <div className="flex">
+            {abilities.map((ability, index) => (
+              <img
+                key={index}
+                src={ability.img}
+                className={`w-10 h-10 mr-7 hover:opacity-100 cursor-pointer abilityIcons ${
+                  playingAbilityIndex === index ? "opacity-100" : "opacity-60"
+                }`}
+                onClick={() => handleAbilityClick(index)}
+              />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="text-white descriptionContainer">
-        <h1 className="text-3xl mb-3 videoTitle">{videoTitle}</h1>
-        <p className="text-base mb-6 videoDescription">{videoDescription}</p>
-        <div className="video-container">
-          {!playingAbilityIndex && !initialVideoPlayed && (
-            <video controls poster={thumbVid}>
-              <source src={videoSource} type="video/mp4" />
-            </video>
-          )}
-          {playingAbilityIndex !== null && (
-            <video
-              autoPlay
-              playsInline
-              loop
-              src={abilities[playingAbilityIndex].video}
-              onEnded={handleVideoEnded}
-            ></video>
-          )}
+        <div className="text-white descriptionContainer">
+          <h1 className="text-3xl mb-3 videoTitle">{videoTitle}</h1>
+          <p className="text-base mb-6 videoDescription">{videoDescription}</p>
+          <div className="video-container">
+            {!playingAbilityIndex && !initialVideoPlayed && (
+              <video controls poster={thumbVid}>
+                <source src={videoSource} type="video/mp4" />
+              </video>
+            )}
+            {playingAbilityIndex !== null && (
+              <video
+                autoPlay
+                playsInline
+                loop
+                src={abilities[playingAbilityIndex].video}
+                onEnded={handleVideoEnded}
+              ></video>
+            )}
+          </div>
         </div>
       </div>
     </div>
